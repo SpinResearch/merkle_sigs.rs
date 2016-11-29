9db0a75bc4f1a8168cd0fed58997d18524b4f924
Romain Ruetschi <romain.ruetschi@gmail.com>
2016-11-29 16:19:32 +0100

    Improve error-handling.

--- src/signatures.rs
+++ src/signatures.rs
@@ -7,51 +7,60 @@ use std::io::{Error, ErrorKind};
 pub type MerkleSignature<D> = (LamportSignatureData, Proof<D, PublicKey<D>>);
 pub type MerkleSignedData<D, T> = (Vec<T>, MerkleSignature<D>);
 
-// TODO: Error management. Currently forcing unwrap within the map.
-// We can leave optionals to avoid having panics.
-pub fn sign_data_vec<D: Digest + Clone, T: AsRef<[u8]>>(data: &Vec<T>,
-                                                        digest: D)
-                                                        -> io::Result<Vec<MerkleSignature<D>>> {
-    let mut leaf_keys: Vec<PrivateKey<D>> = vec![PrivateKey::new(digest.clone()); data.len()];
+fn new_err(reason: &str) -> Error {
+    Error::new(ErrorKind::Other, format!("A signature could not be produced because {}", reason))
+}
+
+pub fn sign_data_vec<D, T>(data: &Vec<T>, digest: D) -> io::Result<Vec<MerkleSignature<D>>>
+        where D: Digest + Clone, T: AsRef<[u8]> {
+
+    let mut leaf_keys = vec![PrivateKey::new(digest.clone()); data.len()];
     let leaf_pub_keys = leaf_keys.iter()
         .map(|priv_key| priv_key.public_key())
         .collect::<Vec<_>>();
 
-    let tree_opt =  MerkleTree::from_vec(digest, leaf_pub_keys.clone());
+    let tree_opt = MerkleTree::from_vec(digest, leaf_pub_keys.clone());
 
     if tree_opt.is_none() {
-        return Err(Error::new(ErrorKind::Other,
-        "A signature could not be produced because an issue occured generating the signing tree."));
+        return Err(new_err("an issue occured while generating the signing tree."));
     }
 
     let tree = tree_opt.unwrap();
 
-    let proofs = leaf_pub_keys.into_iter()
-        .map(|pub_key| (tree.gen_proof(pub_key).unwrap()))
-        .collect::<Vec<_>>();
+    let proofs_opt = leaf_pub_keys.into_iter()
+        .map(|pub_key| tree.gen_proof(pub_key))
+        .collect::<Option<Vec<_>>>();
 
-    let signatures = leaf_keys.iter_mut()
+    let signatures_opt = leaf_keys.iter_mut()
         .zip(data.iter())
-        .map(|(mut priv_key, data)| priv_key.sign(data.as_ref()).unwrap())
-        .collect::<Vec<_>>();
+        .map(|(mut priv_key, data)| priv_key.sign(data.as_ref()))
+        .collect::<Result<Vec<_>, _>>();
 
-    Ok(signatures.into_iter()
-              .zip(proofs)
-              .map(|(sigs, proof)| (sigs, proof))
-              .collect::<Vec<_>>())
+    match (signatures_opt, proofs_opt) {
+        (_, None) =>
+            Err(new_err("an issue occured while generating the inclusion proofs.")),
+
+        (Err(err), _) =>
+            Err(new_err(&format!("an issue occured while signing the data: {}", err))),
+
+        (Ok(signatures), Some(proofs)) =>
+            Ok(signatures.into_iter().zip(proofs).collect())
+    }
 }
 
 /// Verifies the signature of the data. Returns an error if data couldn't be verified.
-pub fn verify_data_vec_signature<D: Digest+Clone, T: Into<Vec<u8>>>(data: T,
-    signature: &MerkleSignature<D>, root_hash: &Vec<u8>) -> io::Result<()>{
+pub fn verify_data_vec_signature<D, T>(data: T, signature: &MerkleSignature<D>, root_hash: &Vec<u8>) -> io::Result<()>
+        where D: Digest + Clone, T: Into<Vec<u8>> {
+
     let (ref sig, ref proof) = *signature;
 
     let valid_root = proof.validate(root_hash);
-    let data_vec: Vec<u8> = data.into();
-    let valid_sig = proof.value.verify_signature(sig, data_vec.as_slice());
+    let data_vec   = data.into() as Vec<u8>;
+    let valid_sig  = proof.value.verify_signature(sig, data_vec.as_slice());
 
     if !valid_root {
-        return Err(Error::new(ErrorKind::Other, "The inclusion proof failed to validate."));
+        return Err(Error::new(ErrorKind::Other,
+                              "The inclusion proof failed to validate."));
     }
 
     if !valid_sig {
