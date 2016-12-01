e1078ccca0d0107625eac3787f614a3aafa39e38
Frederic Jacobs <github@fredericjacobs.com>
2016-12-01 21:04:16 +0100

    Cleaning up imports.

--- src/lib.rs
+++ src/lib.rs
@@ -8,17 +8,30 @@
 
 //! `merkle_sigs` implements Merkle signatures in Rust.
 
+#![deny(
+    // missing_docs,
+    missing_debug_implementations, missing_copy_implementations,
+    trivial_casts, trivial_numeric_casts,
+    unsafe_code, unstable_features,
+    unused_import_braces, unused_qualifications
+)]
+
 #[doc(no_inline)]
 extern crate lamport_sigs;
 extern crate merkle;
 extern crate ring;
 
 mod signatures;
-pub use signatures::{MerkleSignature, MerkleSignedData, verify_data_vec_signature, sign_data_vec};
 
 pub use merkle::Proof;
 
 pub use lamport_sigs::PublicKey;
+pub use signatures::{
+    MerkleSignature,
+    MerkleSignedData,
+    verify_data_vec_signature,
+    sign_data_vec
+};
 
 #[cfg(test)]
 mod tests;
