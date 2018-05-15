9ab9bcf0c3f048b7d57de2f0b54a51d72fa88298
Romain Ruetschi <romain.ruetschi@gmail.com>
2018-05-15 18:23:08 +0200

    Update documentation.

--- implementors/core/convert/trait.Into.js
+++ implementors/core/convert/trait.Into.js
@@ -1,6 +1,6 @@
 (function() {var implementors = {};
 implementors["libc"] = [];
-implementors["merkle_sigs"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.Into.html\" title=\"trait core::convert::Into\">Into</a>&lt;<a class=\"struct\" href=\"https://doc.rust-lang.org/nightly/alloc/vec/struct.Vec.html\" title=\"struct alloc::vec::Vec\">Vec</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.u8.html\">u8</a>&gt;&gt; for <a class=\"struct\" href=\"merkle_sigs/struct.MerklePublicKey.html\" title=\"struct merkle_sigs::MerklePublicKey\">MerklePublicKey</a>",];
+implementors["merkle_sigs"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.Into.html\" title=\"trait core::convert::Into\">Into</a>&lt;<a class=\"struct\" href=\"https://doc.rust-lang.org/nightly/alloc/vec/struct.Vec.html\" title=\"struct alloc::vec::Vec\">Vec</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.u8.html\">u8</a>&gt;&gt; for <a class=\"struct\" href=\"merkle_sigs/struct.MerklePublicKey.html\" title=\"struct merkle_sigs::MerklePublicKey\">MerklePublicKey</a>",synthetic:false,types:["merkle_sigs::signatures::MerklePublicKey"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- implementors/merkle/hashutils/trait.Hashable.js
+++ implementors/merkle/hashutils/trait.Hashable.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["merkle_sigs"] = ["impl <a class=\"trait\" href=\"merkle/hashutils/trait.Hashable.html\" title=\"trait merkle::hashutils::Hashable\">Hashable</a> for <a class=\"struct\" href=\"merkle_sigs/struct.MerklePublicKey.html\" title=\"struct merkle_sigs::MerklePublicKey\">MerklePublicKey</a>",];
+implementors["merkle_sigs"] = [{text:"impl <a class=\"trait\" href=\"merkle/hashutils/trait.Hashable.html\" title=\"trait merkle::hashutils::Hashable\">Hashable</a> for <a class=\"struct\" href=\"merkle_sigs/struct.MerklePublicKey.html\" title=\"struct merkle_sigs::MerklePublicKey\">MerklePublicKey</a>",synthetic:false,types:["merkle_sigs::signatures::MerklePublicKey"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
