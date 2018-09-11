485372fbb8fdf47e8a6733aa1c302055dd49f7fe
Romain Ruetschi <romain.ruetschi@gmail.com>
2018-09-11 10:54:43 +0200

    Formatting

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,12 @@
 #![deny(
-    missing_docs, missing_debug_implementations, missing_copy_implementations, trivial_casts,
-    trivial_numeric_casts, unsafe_code, unstable_features, unused_import_braces,
+    missing_docs,
+    missing_debug_implementations,
+    missing_copy_implementations,
+    trivial_casts,
+    trivial_numeric_casts,
+    unsafe_code,
+    unstable_features,
+    unused_import_braces,
     unused_qualifications
 )]
 
